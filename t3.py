869da0cd05919452ca1c092162540473d3ff5df1
Rodrigotxs <38410498+Rodrigotxs@users.noreply.github.com>
2020-10-05 23:07:56 -0300

    Add files via upload

--- t3.py
+++ t3.py
@@ -14,13 +14,95 @@ DEVICE_LABEL = "Pad"  # Put your device label here
 VARIABLE_LABEL_1 = "gravacao"  # Put your first variable label here
 L=[]
 
+def build_payload(variable):
+    # Creates two random values for sending data
+    variavel = comunicacaoSerial.readline()  
+    variavel = variavel.replace(" ","")
+    variavel = variavel.replace("\0","")
+    variavel = variavel.replace("\n","")
+    variavel = variavel.replace("\r","")
 
-if __name__ == '__main__':
-    while (True):
-        main()
-        time.sleep(1)
+    for tecla in variavel:
+        print(tecla)
+        letra=""
+        numero=""
+        if tecla in ("A","C"):
+          letra = tecla
+        else:
+          numero = tecla
 
-def post_request(gravacao):
+
+
+        if "A" == letra:
+            L=[]
+          
+          
+        if "C" == letra:
+            print(L)
+            payload = {variable: L}
+            return payload
+            reproduzir(L)
+
+
+
+        if "1" == numero:
+            pygame.init()
+            pygame.mixer.music.load('kick1.mp3')
+            pygame.mixer.music.play()
+            L.append(numero)
+                
+                    
+        if "2" == numero:
+            pygame.init()
+            pygame.mixer.music.load('kick2.mp3')
+            pygame.mixer.music.play()
+            L.append(numero)
+            
+        if "3" == numero:
+            pygame.init()
+            pygame.mixer.music.load('kick3.mp3')
+            pygame.mixer.music.play()
+            L.append(numero)
+            
+        if "4" == numero:
+            pygame.init()
+            pygame.mixer.music.load('Crash.mp3')
+            pygame.mixer.music.play()
+            L.append(numero)
+            
+        if "5" == numero:
+            pygame.init()
+            pygame.mixer.music.load('Clap.mp3')
+            pygame.mixer.music.play()
+            L.append(numero)
+            
+        if "6" == numero:
+            pygame.init()
+            pygame.mixer.music.load('cymbal.mp3')
+            pygame.mixer.music.play()
+            L.append(numero)
+            
+        if "7" == numero:
+            pygame.init()
+            pygame.mixer.music.load('Reverb.mp3')
+            pygame.mixer.music.play()
+            L.append(numero)
+            
+        if "8" == numero:
+            pygame.init()
+            pygame.mixer.music.load('Snare.mp3')
+            pygame.mixer.music.play()
+            L.append(numero)
+            
+        if "9" == numero:
+            pygame.init()
+            pygame.mixer.music.load('sirene.mp3')
+            pygame.mixer.music.play()    
+            L.append(numero)
+    
+
+
+def post_request(L):
     # Creates the headers for the HTTP requests
     url = "http://industrial.api.ubidots.com"
     url = "{}/api/v1.6/devices/{}".format(url, DEVICE_LABEL)
@@ -30,7 +112,7 @@ def post_request(gravacao):
     status = 400
     attempts = 0
     while status >= 400 and attempts <= 5:
-        req = requests.post(url=url, headers=headers, json=gravacao)
+        req = requests.post(url=url, headers=headers, json=L)
         status = req.status_code
         attempts += 1
         time.sleep(1)
@@ -43,94 +125,17 @@ def post_request(gravacao):
 
     print("[INFO] request made properly, your device is updated")
     return True
-            
+
+
 def main():
-    variavel = comunicacaoSerial.readline()  
-    variavel = variavel.replace(" ","")
-    variavel = variavel.replace("\0","")
-    variavel = variavel.replace("\n","")
-    variavel = variavel.replace("\r","")
+    payload = build_payload(VARIABLE_LABEL_1)
 
-    for tecla in variavel:
-      print(tecla)
-      letra=""
-      numero=""
-      if tecla in ("A","C"):
-          letra = tecla
-      else:
-          numero = tecla
-        
+    print("[INFO] Attemping to send data")
+    post_request(payload)
+    print("[INFO] finished")
 
-        
-    if "A" == letra:
-        L=[]
-      
-      
-    if "C" == letra:
-        print(L)
-        print("[INFO] Attemping to send data")
-        post_request(L)
-        print("[INFO] finished")
-        reproduzir(L)
-    
-    
-    
-    if "1" == numero:
-        pygame.init()
-        pygame.mixer.music.load('kick1.mp3')
-        pygame.mixer.music.play()
-        L.append(numero)
-            
-                
-    if "2" == numero:
-        pygame.init()
-        pygame.mixer.music.load('kick2.mp3')
-        pygame.mixer.music.play()
-        L.append(numero)
-        
-    if "3" == numero:
-        pygame.init()
-        pygame.mixer.music.load('kick3.mp3')
-        pygame.mixer.music.play()
-        L.append(numero)
-        
-    if "4" == numero:
-        pygame.init()
-        pygame.mixer.music.load('Crash.mp3')
-        pygame.mixer.music.play()
-        L.append(numero)
-        
-    if "5" == numero:
-        pygame.init()
-        pygame.mixer.music.load('Clap.mp3')
-        pygame.mixer.music.play()
-        L.append(numero)
-        
-    if "6" == numero:
-        pygame.init()
-        pygame.mixer.music.load('cymbal.mp3')
-        pygame.mixer.music.play()
-        L.append(numero)
-        
-    if "7" == numero:
-        pygame.init()
-        pygame.mixer.music.load('Reverb.mp3')
-        pygame.mixer.music.play()
-        L.append(numero)
-        
-    if "8" == numero:
-        pygame.init()
-        pygame.mixer.music.load('Snare.mp3')
-        pygame.mixer.music.play()
-        L.append(numero)
-        
-    if "9" == numero:
-        pygame.init()
-        pygame.mixer.music.load('sirene.mp3')
-        pygame.mixer.music.play()    
-        L.append(numero)      
-                
-            
+
+
 def reproduzir(L):
     
     for numero in L:
@@ -201,3 +206,8 @@ def reproduzir(L):
             while pygame.mixer.music.get_busy():
                 pass
                
+
+if __name__ == '__main__':
+    while (True):
+        main()
+        time.sleep(1)
